8828ae932aedd569171b3e08db512c4736c43bb9
fares <troll.rar@gmail.com>
2020-10-03 19:32:18 +0200

    jeej

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'kalkulator';
+  public tt: number;
+  public tv: number;
+  public itm: number;
+
+  public formulaZaIzracun(): void {
+    this.itm = this.tt / (this.tv ** 2);
+  }
 }
